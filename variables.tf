3c20b443499846103f0cad7f1dab6acc9164fb70
Basareom <ombasare@gmail.com>
2026-03-19 22:59:18 +0530

    actions

--- variables.tf
+++ variables.tf
@@ -26,3 +26,9 @@ variable "igw_tags" {
   type        = map(string)
   default = {}
 }
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]     
+}
