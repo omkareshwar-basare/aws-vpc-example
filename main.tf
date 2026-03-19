4d88a6a5b05f625290b1232cd8a14e99e36bb4b9
Basareom <ombasare@gmail.com>
2026-03-19 23:31:25 +0530

    actions

--- main.tf
+++ main.tf
@@ -13,15 +13,18 @@ resource "aws_internet_gateway" "gw" {
   tags = local.igw_tags
 }   
 
-#public subnet
-/*
+#public subnets
+
 resource "aws_subnet" "public" {
   count = length(var.public_subnet_cidrs)
   vpc_id     = aws_vpc.main.id
   cidr_block = var.public_subnet_cidrs[count.index]
-
-  tags = {
-    Name = "Main"
-  }
+  availability_zone = local.az_names[count.index]
+  map_public_ip_on_launch = true
+  tags = merge(
+    {
+      Name = "${local.project}-${local.environment}-public-subnet-${local.az_names[count.index]}"
+    },
+    var.public_subnet_tags
+  )
 }
-*/
--- variables.tf
+++ variables.tf
@@ -32,3 +32,9 @@ variable "public_subnet_cidrs" {
   type        = list(string)
   default     = ["10.0.1.0/24", "10.0.2.0/24"]     
 }
+
+variable "public_subnet_tags" {
+  description = "Tags for public subnets"
+  type        = map(string)
+  default     = {}
+}
